aa2dacfbe9bab2b874e07bb361f0bb7e9c568c1b
Lucia Pol Cerdera <lucipol-@c2r8s5.42malaga.com>
2025-05-16 15:52:16 +0200

    3rd commit

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lucipol- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 12:38:34 by lucipol-          #+#    #+#             */
-/*   Updated: 2025/05/16 12:51:10 by lucipol-         ###   ########.fr       */
+/*   Updated: 2025/05/16 15:50:21 by lucipol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,34 +34,35 @@ char	*ft_itoa(int n)
 	int		size;
 	char	*itoa_number;
 	int  num;
-
-	num = 0;
-	size = get_int_len(num);
+	
+	size = get_int_len(n);
 	itoa_number = (char *)malloc(sizeof(char) * (size + 1));
 	if (!itoa_number)
 		return (NULL);
+	itoa_number[size] = '\0';
+	num = n;
 	if (num == 0)
 		itoa_number[0] = '0';
 	if (num < 0)
+	{
 		itoa_number[0] = '-';
-	num = size -1;
+		num = -num;
+	}
 	while (num != 0)
 	{
-    if (n < 0)
+    //if (n < 0)
 	      itoa_number[--size] = (num % 10) + '0';
 		num = num / 10;
 	}
-	itoa_number[size] = '\0';
 	return (itoa_number);
 }
-
 
 int	main(void)
 {
 	int		number;
 	char	*result;
 
-	number = -2147483648;  // INT_MIN
+	number = 7637828;  // INT_MIN
 	result = ft_itoa(number);
 	if (result)
 	{
